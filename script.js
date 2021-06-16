363548561a75f15b122d5bb3cb1c271aecff5329
Isela <isela.v.alarcon@gmail.com>
2021-06-16 22:17:40 +0200

    starting the pig game project part 2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 'use strict';
 
-const score0 = document.querySelector('#score--0');
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+diceEl.classList.add('hidden');
