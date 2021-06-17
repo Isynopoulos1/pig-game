c3127f6df0103bd0616cd61eadb34dd33f66884e
Isela <isela.v.alarcon@gmail.com>
2021-06-17 21:51:44 +0200

    rolling the dice part 2

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 //Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -14,14 +16,22 @@ score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
 //Rolling dice funtionality
 btnRoll.addEventListener('click', function () {
   // 1.-Generating random dice roll
   const dice = Math.trunc(Math.random() * 6 + 1);
+
   // 2.- Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-  console.log(dice);
 
-  // 3.- Check for rolled 1: if its true, switch for next player
+  // 3.- Check for rolled 1: if its true,
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //CHANGE LATER
+  } else {
+    //switch for next player
+  }
 });
