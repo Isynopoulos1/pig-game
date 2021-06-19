00226e72c694551cfb78fb16c4b9f3163451923f
Isela <isela.v.alarcon@gmail.com>
2021-06-19 21:03:30 +0200

    switching the active player part 2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -36,6 +38,9 @@ btnRoll.addEventListener('click', function () {
     //CHANGE LATER
   } else {
     //switch for next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
