63a4570fe3a6ed014c2e813c41af6f4653a0550c
Isela <isela.v.alarcon@gmail.com>
2021-06-20 21:10:38 +0200

    holding current score part 1

--- script.js
+++ script.js
@@ -17,9 +17,16 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
-
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 //Rolling dice funtionality
 btnRoll.addEventListener('click', function () {
   // 1.-Generating random dice roll
@@ -38,9 +45,22 @@ btnRoll.addEventListener('click', function () {
     //CHANGE LATER
   } else {
     //switch for next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  //1.- add current score to active player's score
+  scores[activePlayer] += currentScore;
+
+  // osea si : score  [1] = score  [1] + current score
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //2.- check if players score is >=100
+  //finish the game
+  //swith to the next player
+  switchPlayer();
 });
